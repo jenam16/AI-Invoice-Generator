511f46b3cda707999a18f82d3ec4249ca1e48769
Jenam Jain <jenam.jain.2006@gmail.com>
2026-08-13 00:51:24 +0530

    added invoice controller

--- server/config/db.js
+++ server/config/db.js
@@ -4,9 +4,9 @@ export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("✅ MongoDB Connected Successfully");
+    console.log(" MongoDB Connected Successfully");
   } catch (error) {
-    console.log("❌ MongoDB Connection Failed:", error.message);
+    console.log(" MongoDB Connection Failed:", error.message);
     process.exit(1);
   }
 };
